8097beee049c0773849f7c381c6357fca26f7c64
Collin Edwards <collinedwards1579@gmail.com>
2018-01-30 20:11:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ return document.querySelector(selector)
 function nestedTarget(){
  return document.getElementById("nested").querySelector(".target")
 }
+
+function increaseRankBy(n){
+var values = parseInt(document.querySelector(".ranked-list"))
+return values + n
+}
